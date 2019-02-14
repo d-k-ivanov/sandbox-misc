785dd31ea80951d1e156ff9f02528f268e206299
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-02-15 02:48:04 +0300

    chnge '-' with '_' in file name

--- cpp/cpp17indetail/build-clang.ps1
+++ cpp/cpp17indetail/build-clang.ps1
@@ -6,7 +6,7 @@ clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Releas
 clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/0_quick_start/print_demo.exe src/0_quick_start/print_demo.cpp
 
 New-Item -Path bin/Release/1_fixes_and_deprecations -ItemType Directory -Force
-clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/1_fixes_and_deprecations/direct-list-initialisation.exe src/1_fixes_and_deprecations/direct-list-initialisation.cpp
+clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/1_fixes_and_deprecations/direct_list_initialisation.exe src/1_fixes_and_deprecations/direct_list_initialisation.cpp
 clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/1_fixes_and_deprecations/silent_assert.exe              src/1_fixes_and_deprecations/silent_assert.cpp
 clang --std=c++14 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/1_fixes_and_deprecations/for_range_loop_14_1.exe        src/1_fixes_and_deprecations/for_range_loop_14_1.cpp
 clang --std=c++17 -O3 -DNDEBUG -Xclang -flto-visibility-public-std -o bin/Release/1_fixes_and_deprecations/for_range_loop_17_1.exe        src/1_fixes_and_deprecations/for_range_loop_17_1.cpp
--- cpp/cpp17indetail/build-mingw.ps1
+++ cpp/cpp17indetail/build-mingw.ps1
@@ -6,7 +6,7 @@ g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/0_quick_start/map_demo.exe   src/
 g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/0_quick_start/print_demo.exe src/0_quick_start/print_demo.cpp -mconsole -lgdi32
 
 New-Item -Path bin/Release/1_fixes_and_deprecations -ItemType Directory -Force
-g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/1_fixes_and_deprecations/direct-list-initialisation.exe src/1_fixes_and_deprecations/direct-list-initialisation.cpp -mconsole -lgdi32
+g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/1_fixes_and_deprecations/direct_list_initialisation.exe src/1_fixes_and_deprecations/direct_list_initialisation.cpp -mconsole -lgdi32
 g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/1_fixes_and_deprecations/silent_assert.exe              src/1_fixes_and_deprecations/silent_assert.cpp              -mconsole -lgdi32
 g++ --std=c++14 -O3 -DNDEBUG -s -o bin/Release/1_fixes_and_deprecations/for_range_loop_14_1.exe        src/1_fixes_and_deprecations/for_range_loop_14_1.cpp        -mconsole -lgdi32
 g++ --std=c++17 -O3 -DNDEBUG -s -o bin/Release/1_fixes_and_deprecations/for_range_loop_17_1.exe        src/1_fixes_and_deprecations/for_range_loop_17_1.cpp        -mconsole -lgdi32
