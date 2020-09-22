2601d7a7c891aa6b12239bd04beacb3061f19488
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-09-23 01:56:49 +0300

    pulumi add static web site for csharp

--- pulumi-dotnet-01/MyStack.cs
+++ pulumi-dotnet-01/MyStack.cs
@@ -1,5 +1,6 @@
 using Pulumi;
 using Pulumi.Aws.S3;
+using Pulumi.Aws.S3.Inputs;
 
 using System.IO;
 
@@ -10,16 +11,21 @@ class MyStack : Stack
         // Create an AWS resource (S3 Bucket)
         var bucket = new Bucket("pulumi-dotnet-01", new BucketArgs
         {
-            Acl = "private",
+            // Acl = "private",
             Tags =
             {
                 { "Environment", "Dev" },
                 { "Name", "pulumi-dotnet-01" },
             },
+            Website = new BucketWebsiteArgs
+            {
+                IndexDocument = "index.html"
+            },
         });
 
         // Export the name of the bucket
         this.BucketName = bucket.Id;
+        this.BucketEndpoint = Output.Format($"http://{bucket.WebsiteEndpoint}");
 
         var filePath        = Path.GetFullPath("./site/index.html");
         var hmtlString      = File.ReadAllText(filePath);
@@ -27,9 +33,11 @@ class MyStack : Stack
         {
             Bucket = bucket.BucketName,
             Content = hmtlString,
+            Acl = "public-read",
+            ContentType = "text/html",
         });
     }
 
-    [Output]
-    public Output<string> BucketName { get; set; }
+    [Output] public Output<string> BucketName       { get; set; }
+    [Output] public Output<string> BucketEndpoint   { get; set; }
 }
