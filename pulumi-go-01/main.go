8faead7e262a87294c3d034959c657e89add62e5
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-09-23 01:53:09 +0300

    pulumi add static web site

--- pulumi-go-01/main.go
+++ pulumi-go-01/main.go
@@ -10,13 +10,19 @@ import (
 func main() {
     pulumi.Run(func(ctx *pulumi.Context) error {
         // Create an AWS resource (S3 Bucket)
-        bucket, err := s3.NewBucket(ctx, "pulumi-go-01", nil)
-        if err != nil {
-            return err
-        }
-
+        bucket, err := s3.NewBucket(ctx, "pulumi-go-01", &s3.BucketArgs{
+            // Acl: pulumi.String("private"),
+            // Tags: {
+            //     { "Environment", "Dev" },
+            //     { "Name", "pulumi-dotnet-01" },
+            // },
+            Website: s3.BucketWebsiteArgs{
+                IndexDocument: pulumi.String("index.html"),
+            },
+        })
         // Export the name of the bucket
         ctx.Export("bucketName", bucket.ID())
+        ctx.Export("bucketEndpoint", pulumi.Sprintf("http://%s", bucket.WebsiteEndpoint))
 
         htmlContent, err := ioutil.ReadFile("site/index.html")
         if err != nil {
@@ -26,10 +32,13 @@ func main() {
         _, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
             Bucket:  bucket.ID(),
             Content: pulumi.String(string(htmlContent)),
+            Acl:         pulumi.String("public-read"),
+            ContentType: pulumi.String("text/html"),
         })
         if err != nil {
             return err
         }
+
 
         return nil
     })
