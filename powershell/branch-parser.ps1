9b0be89fb581ebb1ad5781f864207c91fb476bdc
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-12-21 21:51:22 +0300

    powershell tester: more branches

--- powershell/branch-parser.ps1
+++ powershell/branch-parser.ps1
@@ -1,24 +1,37 @@
-$branch      = "dev/r12_3/divanov/TEST-Task"
-# $branch      = "dev/main/divanov/TEST-Task"
-# $branch      = "dev/abrakadabra/divanov/TEST-Task"
-$targetBranch = ($branch -split '/')[1]
-$checkIsDev = ($branch -split '/')[0]
-$target      = "main"
+$branches = @(
+    "dev/r12_3/divanov/TEST-Task"
+    "dev/releases/r12_3/divanov/TEST-Task"
+    "dev/dev/r12_3/divanov/TEST-Task"
+    "dev/main/divanov/TEST-Task"
+    "dev/main/divanov/divanov/TEST-Task"
+    "dev/abrakadabra/divanov/TEST-Task"
+    "abrakadabra/divanov/TEST-Task"
+    "dev"
+)
 
-if ($checkIsDev -eq 'dev') {
-    if ($targetBranch -eq 'main')
-    {
-        $target = "main"
-    }
-    elseif  ($targetBranch -match  "r\d{1,2}_\d{1}" )
-    {
-        $target = "releases/$targetBranch"
-    }
-}
-else
+foreach ($branch in $branches)
 {
-    Write-Output "Unsupported branch. We run SonarCloud for dev/main and dev/rXX_X"
-}
+    Write-Host "Branch: ${branch}" -ForegroundColor Green
 
-Write-Host "${branch}" -ForegroundColor Green
-Write-Host "${target}" -ForegroundColor Green
+    $targetBranch = ($branch -split '/')[1]
+    $checkIsDev = ($branch -split '/')[0]
+    $target      = "main"
+
+    if ($checkIsDev -eq 'dev') {
+        if ($targetBranch -eq 'main')
+        {
+            $target = "main"
+        }
+        elseif  ($targetBranch -match  "r\d{1,2}_\d{1}" )
+        {
+            $target = "releases/$targetBranch"
+        }
+    }
+    else
+    {
+        Write-Host "`tUnsupported branch. We run SonarCloud for dev/main and dev/rXX_X"  -ForegroundColor Red
+    }
+
+    Write-Host "Target: ${target}" -ForegroundColor Green
+    Write-Host ""
+}
