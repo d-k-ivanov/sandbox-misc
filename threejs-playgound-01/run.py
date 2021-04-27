e09dd57190f538c666141ba3c71593d66d15610b
Dmitry Ivanov <d.k.ivanov@live.com>
2021-04-27 15:12:46 +0200

    Styles++

--- threejs-playgound-01/run.py
+++ threejs-playgound-01/run.py
@@ -10,15 +10,15 @@ PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
-        '.manifest': 'text/cache-manifest',
-	'.html': 'text/html',
-        '.png': 'image/png',
-	'.jpg': 'image/jpg',
-	'.svg':	'image/svg+xml',
-	'.css':	'text/css',
-	'.js':	'application/x-javascript',
-	'': 'application/octet-stream', # Default
-    }
+    '.manifest': 'text/cache-manifest',
+    '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg':	'image/svg+xml',
+    '.css':	'text/css',
+    '.js':	'application/x-javascript',
+    '': 'application/octet-stream', # Default
+}
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
