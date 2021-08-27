47ffd2310ee896ea79507251f04d1a09c5b0c907
Dmitry Ivanov <d.k.ivanov@live.com>
2021-08-27 11:12:09 +0300

    CS 124: playing with tokenization

--- cs124_from_languages_to_information/tokenization.sh
+++ cs124_from_languages_to_information/tokenization.sh
@@ -3,3 +3,16 @@ tr -sc 'A-Za-z' '\n' < ./corpuses/shakespeare.txt | less
 tr -sc 'A-Za-z' '\n' < ./corpuses/shakespeare.txt | sort | less
 tr -sc 'A-Za-z' '\n' < ./corpuses/shakespeare.txt | sort | uniq -c | less
 tr -sc 'A-Za-z' '\n' < ./corpuses/shakespeare.txt | sort | uniq -c | sort -n -r | less
+tr -sc 'A-Za-z' '\n' < ./corpuses/shakespeare.txt | sort | uniq -c | sort -n -r | less
+tr 'A-Z' 'a-z' < ./corpuses/shakespeare.txt | tr -sc 'A-Za-z' '\n' | sort | uniq -c | sort -n -r | less
+
+# Subword tokenization:
+# Byte-Pair Encodinf (BPE)
+# Unigram language modeling tokenization
+# WordPiece
+
+tr 'A-Z' 'a-z' < ./corpuses/low-low-bpe.txt | tr -sc 'A-Za-z' '\n' | sort | uniq -c | sort -n -r | less
+tr 'A-Z' 'a-z' < ./corpuses/shakespeare.txt | sed -r 's/[[:blank:]]+/_/g' | less
+tr 'A-Z' 'a-z' < ./corpuses/shakespeare.txt | sed -r 's/[[:blank:]]+/_/g' | sed 's/./&\n/g' | sort | uniq -c
+tr 'A-Z' 'a-z' < ./corpuses/low-low-bpe.txt | sed -r 's/[[:blank:]]+/_/g' | sed 's/./&\n/g' | sort | uniq -c
+
