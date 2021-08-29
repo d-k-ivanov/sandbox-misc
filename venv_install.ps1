77785e0036322d514a93edadb437e2f5cc59d68b
Dmitry Ivanov <d.k.ivanov@live.com>
2021-08-30 02:40:46 +0300

    CS124: playing with the Naive Bayes

--- venv_install.ps1
+++ venv_install.ps1
@@ -2,8 +2,10 @@
 .\\venv\\Scripts\\activate.ps1
 python -m pip install --upgrade pip
 python -m pip install --upgrade jupyter
+python -m pip install --upgrade matplotlib
 python -m pip install --upgrade numpy
 python -m pip install --upgrade pandas
-python -m pip install --upgrade matplotlib
+python -m pip install --upgrade seaborn
+python -m pip install --upgrade sklearn
 # python -m pip install --upgrade powershell_kernel
 # python -m powershell_kernel.install
