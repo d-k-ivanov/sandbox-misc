acfc424f8d075a0a1c2b7e29eaf1a3043f46b8d4
Dmitry Ivanov <d.k.ivanov@live.com>
2021-09-02 00:52:52 +0300

    CS 124: playing with Word2VecLite

--- venv_install.ps1
+++ venv_install.ps1
@@ -2,11 +2,13 @@
 .\\venv\\Scripts\\activate.ps1
 python -m pip install --upgrade pip
 python -m pip install --upgrade jupyter
+python -m pip install --upgrade Keras
 python -m pip install --upgrade matplotlib
 python -m pip install --upgrade nltk
 python -m pip install --upgrade numpy
 python -m pip install --upgrade pandas
 python -m pip install --upgrade seaborn
 python -m pip install --upgrade sklearn
+python -m pip install --upgrade tensorflow
 # python -m pip install --upgrade powershell_kernel
 # python -m powershell_kernel.install
